bee72788980fa9d8fb0cf70314f34435fb85917f
Codegainers <codegainers@gmail.com>
2025-07-18 12:04:22 +0200

    password limit

--- validation.py
+++ validation.py
@@ -4,8 +4,6 @@ def is_valid_password(password):
     # At least 8 characters
     if len(password) < 8:
         return False, "Password must be at least 8 characters long."
-    if len(password) >20:
-        return False, "Password too long."
     # At least one uppercase letter
     if not re.search(r'[A-Z]', password):
         return False, "Password must contain at least one uppercase letter."
